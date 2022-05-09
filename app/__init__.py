bdcd1eae499a441e8e814eb66675952daa418338
cristobal_javier@hotmail.com <cristobal_javier@hotmail.com>
2022-05-09 17:43:44 -0400

    Added flask cors

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,12 @@
 from flask import Flask
+from flask_cors import CORS
+
 
 
 def create_app():
     app = Flask(__name__)
+    CORS(app)
+    app.config['CORS_HEADERS'] = 'Content-Type'
 
     from . import recommender_app
 
