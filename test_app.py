d9795bae2bfa0d119a6e3cd7a57b58e8f4041fc0
cristobal_javier@hotmail.com <cristobal_javier@hotmail.com>
2022-05-10 16:28:36 -0400

    changed to 100 tests

--- test_app.py
+++ test_app.py
@@ -12,7 +12,7 @@ def test_app():
     c = app.test_client()
     c.get("/train")
 
-    for i in np.random.randint(0, df.shape[0], 10):
+    for i in np.random.randint(0, df.shape[0], 100):
         url = df["url"].iloc[i]
         response = c.get(f"/reco?url={url}")
         data = json.loads(response.get_data())["recommendations"]
